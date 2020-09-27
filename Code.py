77c8e9b97397afdeefafdca38e79b580b290b573
shreyakalla <71296212+shreyakalla@users.noreply.github.com>
2020-09-27 15:39:47 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -4,12 +4,9 @@ day = input("What day were you born:")
 month = int(month)
 day = int(day)
 String season = ""
-if(12 <= month <= 2)
-  season = "Winter Baby"
-if(3 <= month <= 5)
-  season = "Spring Baby"
+
 if(6 <= month <=8)
   season = "Summer Baby"
-if(9 <= 11)
+if(9 <=month <= 11)
   season = "Fall Baby"
 print("Hi " , name , ", you are born on " , month, " ", day, "! You are a ", season, "!")
